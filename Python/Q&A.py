3542882c18aa9c87ea70ffdd9ee09fe413ed7c37
pie-bro <55726145+pie-bro@users.noreply.github.com>
2020-09-24 11:12:01 +1200

    Update Q&A.py

--- Python/Q&A.py
+++ Python/Q&A.py
@@ -329,6 +329,7 @@ from random import randint
 random_numbers = [ randint(number, 2 * number) for number in range(10)]
 print(random_numbers)
 
+
 Dictionaries
 
 numbers = {'one':1, 'two':2, 'three':3}
@@ -381,7 +382,57 @@ for planet, initial in planet_to_initial.items():
     Uranus begins with "U"
    Neptune begins with "N"
    
-   
+
+
+Enumerate()
+# Python program to illustrate 
+# enumerate function 
+l1 = ["eat","sleep","repeat"] 
+s1 = "geek"
+  
+# creating enumerate objects 
+obj1 = enumerate(l1) 
+obj2 = enumerate(s1) 
+  
+print "Return type:",type(obj1) 
+print list(enumerate(l1)) 
+  
+# changing start index to 2 from 0 
+print list(enumerate(s1,2)) 
+Output:
+
+Return type: < type 'enumerate' >
+[(0, 'eat'), (1, 'sleep'), (2, 'repeat')]
+[(2, 'g'), (3, 'e'), (4, 'e'), (5, 'k')]
+
+Using Enumerate object in loops
+
+filter_none
+edit
+play_arrow
+
+brightness_4
+# Python program to illustrate 
+# enumerate function in loops 
+l1 = ["eat","sleep","repeat"] 
+  
+# printing the tuples in object directly 
+for ele in enumerate(l1): 
+    print ele 
+print 
+# changing index and printing separately 
+for count,ele in enumerate(l1,100): 
+    print count,ele 
+Output:
+
+(0, 'eat')
+(1, 'sleep')
+(2, 'repeat')
+
+100 eat
+101 sleep
+102 repeat
+
 
 Question
 When should I create a Pandas dataframe using a dictionary or a list?
